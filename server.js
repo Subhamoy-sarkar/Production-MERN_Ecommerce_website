def63868fd479ab0e91218f182168d92330b45c7
Subhamoy Sarkar <subhamoysarkar526@gmail.com>
2024-09-05 15:28:19 +0530

    Update server.js
    Esmodule deployment error fix , __dirname not found is fixed

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import path from 'path';
+import { fileURLToPath } from "url";
 
 
 //Configure env : 
@@ -18,6 +19,10 @@ dotenv.config()
 // database config
 connectDB();
 
+
+//esModule fix , __dirname not defined fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest object
 const app=express();
